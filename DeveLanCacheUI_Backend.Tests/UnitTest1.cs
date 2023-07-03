c938635602f011b1e7539b817d3e34f21f9d7c69
Devedse <davydavidse@gmail.com>
2023-07-03 19:08:37 +0200

    Added test project

---
+++ DeveLanCacheUI_Backend.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace DeveLanCacheUI_Backend.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
