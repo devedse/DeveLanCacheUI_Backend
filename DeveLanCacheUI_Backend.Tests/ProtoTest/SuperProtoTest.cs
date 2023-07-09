6f2505a2e474f21db8d5f0236ab9af487498baf7
Devedse <davydavidse@gmail.com>
2023-07-10 01:41:58 +0200

    Testing with protobuf

---
+++ DeveLanCacheUI_Backend.Tests/ProtoTest/SuperProtoTest.cs
@@ -0,0 +1,20 @@
+﻿using DeveLanCacheUI_Backend.ProtoTest;
+
+namespace DeveLanCacheUI_Backend.Tests.ProtoTest
+{
+    [TestClass]
+    public class SuperProtoTest
+    {
+        [TestMethod]
+        public void GoTest()
+        {
+            var path = Path.Combine("ProtoTest", "10336669592858206477");
+
+            var bytes = File.ReadAllBytes(path);
+            string aaaaBase64 = Convert.ToBase64String(bytes);
+
+            SuperLoader.GoLoad(path);
+
+        }
+    }
+}
---
+++ DeveLanCacheUI_Backend/ProtoTest/SuperLoader.cs
@@ -0,0 +1,20 @@
+﻿using ProtoBuf;
+using SteamKit2;
+using SteamKit2.Internal;
+
+namespace DeveLanCacheUI_Backend.ProtoTest
+{
+    public static class SuperLoader
+    {
+        public static void GoLoad(string filePath)
+        {
+            var allBytes = File.ReadAllBytes(filePath);
+            using var openBytes = File.OpenRead(filePath);
+            //var retval = DepotManifest.Deserialize(allBytes);
+            //var retval = new Steam3Manifest(allBytes);
+            var retval = Serializer.Deserialize<ContentManifestPayload>(openBytes);
+
+            Console.WriteLine(retval);
+        }
+    }
+}
