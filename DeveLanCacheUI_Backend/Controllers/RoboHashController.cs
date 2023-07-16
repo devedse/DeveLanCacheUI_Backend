cb3101a7c041e90fa9b959257dc0edd9d50abce8
Devedse <davydavidse@gmail.com>
2023-07-16 03:53:58 +0200

    add cache

--- DeveLanCacheUI_Backend/Controllers/RoboHashController.cs
+++ DeveLanCacheUI_Backend/Controllers/RoboHashController.cs
@@ -1,6 +1,7 @@
 using DeveHashImageGenerator.RoboHash;
 using Microsoft.AspNetCore.Mvc;
 using SixLabors.ImageSharp;
+using System.Collections.Concurrent;
 
 namespace DeveLanCacheUI_Backend.Controllers
 {
@@ -9,6 +10,7 @@ namespace DeveLanCacheUI_Backend.Controllers
     public class RoboHashController : ControllerBase
     {
         private readonly ILogger<RoboHashController> _logger;
+        private static ConcurrentDictionary<string, byte[]> _imagesCache = new ConcurrentDictionary<string, byte[]>();
 
         public RoboHashController(ILogger<RoboHashController> logger)
         {
@@ -18,31 +20,58 @@ namespace DeveLanCacheUI_Backend.Controllers
         [HttpGet("{text}")]
         public IActionResult GetRoboHash(string text, string? roboSet = null, string? color = null, string? format = "png", string? bgSet = null, int sizeX = 300, int sizeY = 300)
         {
-            try
-            {
-                var roboHashGenerator = new RoboHashGenerator();
-                var image = roboHashGenerator.Assemble(text, roboSet, color, format, bgSet, sizeX, sizeY);
+            byte[] data;
 
-                var outputStream = new MemoryStream();
-                switch (format?.ToLower())
+            var key = $"{text}_{roboSet}_{color}_{format}_{bgSet}_{sizeX}_{sizeY}";
+            if (_imagesCache.TryGetValue(key, out data))
+            {
+                return new FileContentResult(data, GetMimeType(format));
+            }
+            else
+            {
+                try
                 {
-                    case "jpg":
-                        image.SaveAsJpeg(outputStream);
-                        return File(outputStream.ToArray(), "image/jpeg");
-                    case "bmp":
-                        image.SaveAsBmp(outputStream);
-                        return File(outputStream.ToArray(), "image/bmp");
-                    case "png":
-                    default:
-                        image.SaveAsPng(outputStream);
-                        return File(outputStream.ToArray(), "image/png");
+                    var roboHashGenerator = new RoboHashGenerator();
+                    var image = roboHashGenerator.Assemble(text, roboSet, color, format, bgSet, sizeX, sizeY);
+
+                    var outputStream = new MemoryStream();
+                    switch (format?.ToLower())
+                    {
+                        case "jpg":
+                            image.SaveAsJpeg(outputStream);
+                            break;
+                        case "bmp":
+                            image.SaveAsBmp(outputStream);
+                            break;
+                        case "png":
+                        default:
+                            image.SaveAsPng(outputStream);
+                            break;
+                    }
+
+                    data = outputStream.ToArray();
+
+                    _imagesCache.TryAdd(key, data); // Adding to cache
+
+                    return new FileContentResult(data, GetMimeType(format));
+                }
+                catch (Exception e)
+                {
+                    _logger.LogError(e, "An error occurred while generating RoboHash image");
+                    return BadRequest("Failed to generate image");
                 }
             }
-            catch (Exception e)
+        }
+
+        private string GetMimeType(string? format)
+        {
+            return format?.ToLower() switch
             {
-                _logger.LogError(e, "An error occurred while generating RoboHash image");
-                return BadRequest("Failed to generate image");
-            }
+                "jpg" => "image/jpeg",
+                "bmp" => "image/bmp",
+                "png" => "image/png",
+                _ => "application/octet-stream",
+            };
         }
     }
 }
