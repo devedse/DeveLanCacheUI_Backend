14e43ab73e1558608915a692b177f8960ef4b052
Devedse <davydavidse@gmail.com>
2025-05-06 12:34:33 +0200

    A bit of cleanup

--- DeveLanCacheUI_Backend.EpicManifestParser.Tests/EpicManifestParserTests.cs
+++ DeveLanCacheUI_Backend.EpicManifestParser.Tests/EpicManifestParserTests.cs
@@ -1,28 +1,16 @@
-﻿using DeveLanCacheUI_Backend.EpicManifestParser.Decompressor;
-using DeveLanCacheUI_Backend.EpicManifestParser.UE;
-
-namespace DeveLanCacheUI_Backend.EpicManifestParser.Tests
+﻿namespace DeveLanCacheUI_Backend.EpicManifestParser.Tests
 {
     [TestClass]
-    public sealed class EpicManifestParser
+    public sealed class EpicManifestParserTests
     {
         [TestMethod]
         public async Task DoesItWork()
         {
             // Arrange
-            var options = new ManifestParseOptions
-            {
-                //ChunkBaseUrl = "http://download.epicgames.com/Builds/UnrealEngineLauncher/CloudDir/",
-                //ChunkCacheDirectory = Directory.CreateDirectory(Path.Combine(Benchmarks.DownloadsDir, "chunks_v2")).FullName,
-                //ManifestCacheDirectory = Directory.CreateDirectory(Path.Combine(Benchmarks.DownloadsDir, "manifests_v2")).FullName,
-            };
-
-            options.Decompressor = ManifestZlibDotNetDecompressor.Decompress;
-
+            var manifestBuffer = await File.ReadAllBytesAsync(Path.Combine("TestFiles", "1sE9O19OT_X-rOWTFEiMUGNBYu8I1A.manifest"));
 
             // Act
-            var manifestBuffer = await File.ReadAllBytesAsync(Path.Combine("TestFiles", "1sE9O19OT_X-rOWTFEiMUGNBYu8I1A.manifest"));
-            var manifest = FBuildPatchAppManifest.Deserialize(manifestBuffer, options);
+            var manifest = EpicManifestParser.Deserialize(manifestBuffer);
 
             // Assert
             Assert.AreEqual("Super Space Club.exe", manifest.Meta.LaunchExe);
--- DeveLanCacheUI_Backend.EpicManifestParser/EpicManifestParser.cs
+++ DeveLanCacheUI_Backend.EpicManifestParser/EpicManifestParser.cs
@@ -1,7 +1,23 @@
-﻿//namespace DeveLanCacheUI_Backend.EpicManifestParser
-//{
-//    public class EpicManifestParser
-//    {
-//        public async Task<>
-//    }
-//}
+﻿using DeveLanCacheUI_Backend.EpicManifestParser.Decompressor;
+using DeveLanCacheUI_Backend.EpicManifestParser.UE;
+
+namespace DeveLanCacheUI_Backend.EpicManifestParser
+{
+    public class EpicManifestParser
+    {
+        public static FBuildPatchAppManifest Deserialize(ManifestRoData manifestBuffer)
+        {
+            var options = new ManifestParseOptions
+            {
+                //ChunkBaseUrl = "http://download.epicgames.com/Builds/UnrealEngineLauncher/CloudDir/",
+                //ChunkCacheDirectory = Directory.CreateDirectory(Path.Combine(Benchmarks.DownloadsDir, "chunks_v2")).FullName,
+                //ManifestCacheDirectory = Directory.CreateDirectory(Path.Combine(Benchmarks.DownloadsDir, "manifests_v2")).FullName,
+            };
+
+            options.Decompressor = ManifestZlibDotNetDecompressor.Decompress;
+
+            var manifest = FBuildPatchAppManifest.Deserialize(manifestBuffer, options);
+            return manifest;
+        }
+    }
+}
